96889f57d90686a63966059c6e3563c37b53624a
jin.chang <jin.chang@zhangmen.com>
2020-07-30 15:47:49 +0800

    学习基本类型

---
+++ ts-basic-type/type.js
@@ -0,0 +1,5 @@
+// any
+var any = '1';
+var arr = [1, '1'];
+var tuple = ['1', 2];
+console.log(tuple[0]);
---
+++ ts-basic-type/type.ts
@@ -0,0 +1,8 @@
+// any
+const any: any = '1';
+
+const arr: Array<number | string> = [1, '1']
+
+let tuple: [string, number] = ['1', 2]
+
+console.log(tuple[0])
